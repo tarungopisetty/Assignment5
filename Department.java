7bf7cf2b215d19db4dfded7c3ac6aee2581e9a10
tarungopisetty <79128297+tarungopisetty@users.noreply.github.com>
2024-03-25 18:31:04 +0530

    Add files via upload

--- Department.java
+++ Department.java
@@ -14,14 +14,14 @@ public class Department {
 
     public static void main(String[] args) {
         try {
-            // Connect to the database
+            
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/departments", "tharun", " ");
 
-            // Create a PreparedStatement
+            
             String query = "INSERT INTO department (id, name) VALUES (?, ?)";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
 
-            // Insert Department objects into the database
+            
             Department department1 = new Department(1, "HR");
             Department department2 = new Department(2, "IT");
 
@@ -35,7 +35,7 @@ public class Department {
 
             System.out.println("Department objects have been successfully stored in the database.");
 
-            // Close the resources
+            
             preparedStatement.close();
             connection.close();
         } catch (SQLException e) {
